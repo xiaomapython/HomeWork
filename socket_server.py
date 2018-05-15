2563b5620dedc1213b469ba46575c0f651809b40
xiaomapython <38485217+xiaomapython@users.noreply.github.com>
2018-05-15 16:36:32 +0800

    Create socket_server.py

---
+++ socket_server.py
@@ -0,0 +1,22 @@
+# -*- coding=utf8 -*-
+import socket
+server = socket.socket()  # 默认tcp
+
+# 可以修改为UDP协议
+# server = socket.socket(socket.SOCK_DGRAM)
+
+server.bind(("127.0.0.1", 8000))
+print(server)
+server.listen()
+flag = True
+while flag:
+    conn, address = server.accept()
+    while flag:
+        try:
+            msg = conn.recv(1024).decode()
+            print("{} say:".format(address[0]), msg)
+            conn.sendall("收到".encode())
+        except OSError as e:
+            conn.close()
+            print("客户端自动断开连接")
+            flag = False
